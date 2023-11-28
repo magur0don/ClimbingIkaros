0202e4b4af210329ff3e57d508437ce93e90711e
magur0don <magurodon.10000yen@gmail.com>
2023-11-28 21:49:54 +0900

    Scoreのひょうじまで

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public PlayerMovement Player;
+
+    /// <summary>
+    /// åªç›ÇÃÉXÉRÉA
+    /// </summary>
+    private float currentHighScore = 0f;
+
+    public float GetScore
+    {
+        get { return currentHighScore; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHighScore < Player.transform.position.y)
+        {
+            currentHighScore = Player.transform.position.y;
+        }
+
+        Debug.Log(currentHighScore);
+    }
+}
