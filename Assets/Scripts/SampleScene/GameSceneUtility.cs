25f21e8a60eaccaad702b90246eb1d27ddde9558
magur0don <magurodon.10000yen@gmail.com>
2023-12-14 08:32:39 +0900

    宿題まで

---
+++ Assets/Scripts/SampleScene/GameSceneUtility.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームのシーンに関するデータ
+/// </summary>
+public static class GameSceneUtility 
+{
+    /// <summary>
+    /// SampleSceneの名前の定数
+    /// </summary>
+    public const string SampleSceneName = "SampleScene";
+}
