4fd503e21ede63d2c6fae147136620a7808f7871
magur0don <magurodon.10000yen@gmail.com>
2023-12-18 21:48:42 +0900

    宿題まで

--- Assets/Scripts/TitleScene/TitleSceneManager.cs
+++ Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -6,10 +6,26 @@ using UnityEngine.SceneManagement;
 public class TitleSceneManager : MonoBehaviour
 {
     /// <summary>
+    /// soundManagerが使われるタイミングでシーン上から探索する
+    /// </summary>
+    private SoundManager soundManager => FindAnyObjectByType<SoundManager>();
+
+    private void Start()
+    {
+        // soundManagerがnullじゃなければ
+        if (soundManager != null)
+        {
+            // BGMを再生する
+            soundManager.PlayBGM();
+        }
+    }
+    /// <summary>
     /// インゲームのシーンに移行する
     /// </summary>
-    public void GotoInGameScene() 
+    public void GotoInGameScene()
     {
+        // SEを鳴らす
+        soundManager.PlaySE();
         SceneManager.LoadScene(GameSceneUtility.SampleSceneName);
-;   }
+    }
 }
