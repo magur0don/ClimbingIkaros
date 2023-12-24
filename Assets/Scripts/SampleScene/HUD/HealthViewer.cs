ab73a2237ba7ca93ae37228f8e0f62651a0428d3
magur0don <magurodon.10000yen@gmail.com>
2023-12-24 13:14:55 +0900

    10回目の授業終わりまで

--- Assets/Scripts/SampleScene/HUD/HealthViewer.cs
+++ Assets/Scripts/SampleScene/HUD/HealthViewer.cs
@@ -15,15 +15,24 @@ public class HealthViewer : MonoBehaviour
     /// </summary>
     public Slider HealthSlider;
 
+    private void Start()
+    {
+        HealthSlider.value = 1f;
+    }
+
     private void Update()
     {
         // SliderのPositionにカメラの座標変換を使ってプレイヤーのポジション＋Y軸方向に1の値を代入し続ける
         this.HealthSlider.GetComponent<RectTransform>().position =
           Camera.main.WorldToScreenPoint(this.Health.gameObject.transform.position + Vector3.up);
-        
+
         // 死んでいたらSliderに値を代入しないようにする
         if (Health.IsDead)
         {
+            if (HealthSlider.value != 0)
+            {
+                HealthSlider.value = 0;
+            }
             return;
         }
         // 現在体力/最大体力をすることによって、80/100だった場合0.8の値をSliderに与えることができる
--- Assets/Scripts/SampleScene/MainGameStateManager.cs
+++ Assets/Scripts/SampleScene/MainGameStateManager.cs
@@ -8,6 +8,16 @@ public class MainGameStateManager : MonoBehaviour
 
     public Timer Timer;
 
+    public Health PlayerHealth;
+
+    bool gameEnd = false;
+
+    // 外部からgameEndを取得するゲッター
+    public bool GetGameEnd
+    {
+        get { return gameEnd; }
+    }
+
     /// <summary>
     /// soundManagerが使われるタイミングでシーン上から探索する
     /// </summary>
@@ -21,11 +31,24 @@ public class MainGameStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (gameEnd)
+        {
+            return;
+        }
+
+        if (PlayerHealth.IsDead)
+        {
+            // ResultCanvasのアクティブをオンにする
+            ResultCanvas.gameObject.SetActive(true);
+            gameEnd = true;
+        }
+
         // Timerの制限時間が0を下回ったとき
         if (Timer.GetGameTime < 0f)
         {
             // ResultCanvasのアクティブをオンにする
             ResultCanvas.gameObject.SetActive(true);
+            gameEnd = true;
         }
     }
 }
